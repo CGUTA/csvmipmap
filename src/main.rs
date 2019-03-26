2348fe09e7b82f43fce092b3231613b5f8ae0155
Carlos Urzua <urzutrasla@gmail.com>
2019-03-26 13:00:27 +0100

    prototype for hugeheat

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate csv;
 
 use std::error::Error;
-use std::io;
+//use std::io;
 use std::process;
-//use std::env;
+use std::env;
 
 #[derive(Clone)]
 struct Bin {
 	count: u32,
 	total: f64,
+	count_negative: u32,
+	total_negative: f64,
 }
 
 impl Bin {
@@ -16,17 +18,35 @@ impl Bin {
 		Bin {
 			count: 0,
 			total: 0.0,
+			count_negative: 0,
+			total_negative: 0.0,
 		}
 	}
 	
 	fn store(&mut self, value: f64){
-	    self.total += value;
-	    self.count += 1;
+		if value >= 0.0 {
+			self.total += value;
+	    	self.count += 1;
+		} else {
+			self.total_negative += value;
+	    	self.count_negative += 1;
+		}
+	    
 	}
 
-	fn render(& self) -> f64 {
-	    let value = self.total / self.count as f64;
-	    value
+	fn render(& self) -> String {
+		let mut value = 0.0;
+		let mut value_negative = 0.0;
+
+		if self.count > 0 {
+			value = self.total / self.count as f64;
+		}
+	    if self.count_negative > 0 {
+	    	value_negative = self.total_negative / self.count_negative as f64;
+	    }
+
+
+	    format!("{},{}", value, value_negative)
 	}
 	
 }
@@ -61,37 +81,65 @@ impl MapRow{
 	}
 }
 
-fn example() -> Result<(), Box<Error>> {
+fn example(target_file: &str, bin_size: usize) -> Result<(), Box<Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
+        .has_headers(true)
         .delimiter(b'\t')
-        .from_path("../simpleheat/demo/data/TCGA_TACNA_samples_sorted_ward.D2_common_tissue_type.txt")?;
+        .from_path(target_file)?;
         //.from_path("test.tsv")?;
 
     //let mut _total: f64 = 0.0;
 
-    let bin_size = 10;
+    //let bin_size = 10;
+    println!("id,col,value,value_negative");
 
     let columns;
+    let how_many_bins;
+    let mut my_row;
+    let mut row_counter = 0;
     {
 	    let mut iter = rdr.records();
 
 	    if let Some(result) = iter.next() {
 	        let record = result?;
 	        columns = record.len();
+	        how_many_bins = (columns + bin_size - 1)/bin_size;
+	        my_row = MapRow::new(how_many_bins);
+	        for (index_row, field) in record.iter().enumerate()
+	        {
+	        	//let value = field.parse::<f64>().unwrap_or_else(|field| { println!("Value cannot be parsed into float -> {}", field); });
+	        	if index_row > 1{
+	        		let value = field.parse::<f64>().expect("Value cannot be parsed into float");
+
+
+	        	let which_bin = index_row / bin_size; //Floor of integer division
+
+	        	//println!("{}:val{}", which_bin, value);
+
+	        	my_row.update(which_bin, value)
+
+	        	}
+	        	
+
+	        }
+
+	        row_counter += 1;
+
+	        if row_counter % bin_size == 0 {
+	        	my_row.print();
+	        	my_row.erase();
+	        }
 	    } else {
 	    	panic!("No fields!")
 	    }
 	}
 
-    let mut row_counter = 0;
+    
 
-    let how_many_bins = (columns + bin_size - 1)/bin_size;
+    
 
-    let mut my_row = MapRow::new(how_many_bins);
 
-    println!("id,col,value");
 
     for result in rdr.records() {
         // The iterator yields Result<StringRecord, Error>, so we check the
@@ -104,12 +152,17 @@ fn example() -> Result<(), Box<Error>> {
         {
         	//let value = field.parse::<f64>().unwrap_or_else(|field| { println!("Value cannot be parsed into float -> {}", field); });
 
-        	let value = field.parse::<f64>().expect("Value cannot be parsed into float");
+        	if index_row > 1{
+	        		let value = field.parse::<f64>().expect("Value cannot be parsed into float");
 
 
-        	let which_bin = index_row / bin_size; //Floor of integer division
+	        	let which_bin = index_row / bin_size; //Floor of integer division
 
-        	my_row.update(which_bin, value)
+	        	//println!("{}:val{}", which_bin, value);
+
+	        	my_row.update(which_bin, value)
+
+	        	}
 
         }
 
@@ -130,10 +183,11 @@ fn example() -> Result<(), Box<Error>> {
 
 fn main() {
 
-	//let args: Vec<String> = env::args().collect();
-	//let bin_size = &args[1];
+	let args: Vec<String> = env::args().collect();
+	let bin_size = &args[1].parse::<usize>().expect("Invalid bin size");
+	let target_file = &args[2];
 
-    if let Err(err) = example() {
+    if let Err(err) = example(target_file, *bin_size) {
         println!("error running example: {}", err);
         process::exit(1);
     }
